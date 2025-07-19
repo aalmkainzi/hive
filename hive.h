424a5e5cebaed46bf161c1c15eeb5e1edc3002d0
Abdulmalek Almkainzi <aalmkainzi@gmail.com>
2025-07-19 17:01:50 +0300

    removed comment

--- hive.h
+++ hive.h
@@ -14,9 +14,6 @@
 
 #endif
 
-//#if !defined(HIVE_DECLARED)
-//#define HIVE_DECLARED
-
 #define HIVE_BUCKET_SIZE (UINT8_MAX - 1)
 
 #if !defined(HIVE_ALLOC)
@@ -170,8 +167,6 @@ void hive_iter_go_next(hive_iter *_it);
 hive_entry_t *hive_iter_elm(hive_iter _it);
 hive_iter hive_iter_del(HIVE_NAME *_hv, hive_iter _it);
 bool hive_iter_eq(hive_iter _a, hive_iter _b);
-
-//#endif
 
 // #define HIVE_IMPL
 #if defined(HIVE_IMPL)
